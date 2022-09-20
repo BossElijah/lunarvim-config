b7e6f1bbbddce347ff175fe82c2e6a8858370bbc
Elias Bruhn <elias@lfpost.dk>
2022-09-20 10:32:51 +0200

    Minor config changes, cleanup file to use singlequotes

--- config.lua
+++ config.lua
@@ -36,70 +36,72 @@
 
 
 -- General config.
-lvim.log.level = "warn"
+lvim.log.level = 'warn'
 lvim.format_on_save = false
-lvim.colorscheme = "onedarker"
+lvim.colorscheme = 'onedarker'
 
 -- Keymappings.
-lvim.leader = "space"
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.normal_mode["<S-A-Tab>"] = ":tabprevious<cr>"
-lvim.keys.normal_mode["<A-Tab>"] = ":tabnext<cr>"
+lvim.leader = 'space'
+lvim.keys.normal_mode['<C-s>'] = ':w<cr>'
+lvim.keys.normal_mode['<S-A-Tab>'] = ':tabprevious<cr>'
+lvim.keys.normal_mode['<A-Tab>'] = ':tabnext<cr>'
+lvim.keys.normal_mode['H'] = ':bp<cr>'
+lvim.keys.normal_mode['L'] = ':bn<cr>'
 
 -- Danish special characters.
-lvim.keys.insert_mode['<A-\'>'] = "æ"
-lvim.keys.insert_mode['<A-">'] = "Æ"
-lvim.keys.insert_mode['<A-o>'] = "ø"
-lvim.keys.insert_mode['<A-O>'] = "Ø"
-lvim.keys.insert_mode['<A-a>'] = "å"
-lvim.keys.insert_mode['<A-A>'] = "Å"
+lvim.keys.insert_mode['<A-\'>'] = 'æ'
+lvim.keys.insert_mode['<A-">'] = 'Æ'
+lvim.keys.insert_mode['<A-o>'] = 'ø'
+lvim.keys.insert_mode['<A-O>'] = 'Ø'
+lvim.keys.insert_mode['<A-a>'] = 'å'
+lvim.keys.insert_mode['<A-A>'] = 'Å'
 
 -- Custom config for predefined plugins.
 -- After changing plugin config, exit and reopen LunarVim and run :PackerInstall :PackerCompile.
 lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.alpha.mode = 'dashboard'
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.bufferline.options.mode = 'tabs'
 -- nvim-tree.
-lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.view.side = 'left'
 lvim.builtin.nvimtree.setup.view.width = 40
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.nvimtree.setup.actions.open_file.resize_window = true
 lvim.autocommands = {
   {
-    "VimEnter",
+    'VimEnter',
     {
-      pattern = { "*" },
-      command = "NvimTreeOpen"
+      pattern = { '*' },
+      command = 'NvimTreeOpen'
     }
   },
   {
-    "VimEnter",
+    'VimEnter',
     {
-      pattern = { "*" },
-      command = "wincmd p"
+      pattern = { '*' },
+      command = 'wincmd p'
     }
   },
   {
-    "TabNew",
+    'TabNew',
     {
-      pattern = { "*" },
-      command = "NvimTreeOpen"
+      pattern = { '*' },
+      command = 'NvimTreeOpen'
     }
   },
   {
-    "TabNew",
+    'TabNew',
     {
-      pattern = { "*" },
-      command = "wincmd p"
+      pattern = { '*' },
+      command = 'wincmd p'
     }
   }
 }
 -- Telescope.
-lvim.builtin.telescope.defaults.path_display = { "truncate" }
+lvim.builtin.telescope.defaults.path_display = { 'truncate' }
 -- Lualine.
-lvim.builtin.lualine.style = "default"
+lvim.builtin.lualine.style = 'default'
 lvim.builtin.lualine.options.globalstatus = true
 local defaultColors = { bg = '282C33', fg = 'abb2bf' }
 lvim.builtin.lualine.sections = {
@@ -127,70 +129,70 @@ lvim.builtin.lualine.inactive_sections = {
 lvim.builtin.lualine.on_config_done = function(lualine)
   local config = lualine.get_config()
 
-  config.sections.lualine_z[1].color = { bg = "#2c2c2c" }
-  config.sections.lualine_z[3].color = { bg = "#2c2c2c" }
+  config.sections.lualine_z[1].color = { bg = '#2c2c2c' }
+  config.sections.lualine_z[3].color = { bg = '#2c2c2c' }
 
   lualine.setup(config)
 end
 
 -- Treesitter.
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+  'bash',
+  'c',
+  'javascript',
+  'json',
+  'lua',
+  'python',
+  'typescript',
+  'tsx',
+  'css',
+  'rust',
+  'java',
+  'yaml',
 }
-lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.ignore_install = { 'haskell' }
 lvim.builtin.treesitter.highlight.enabled = true
 
 -- Completion.
-local cmp = require'cmp'
-lvim.builtin.cmp.mapping = cmp.mapping.preset.insert({
-  ["<Tab>"] = cmp.mapping(function(fallback)
-    -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item.
-    if cmp.visible() then
-      local entry = cmp.get_selected_entry()
-      if not entry then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-        cmp.confirm()
-      else
-        cmp.confirm()
-      end
-    else
-      fallback()
+local cmp = require 'cmp'
+
+local expandSuggestionMapping = cmp.mapping(function(fallback)
+  -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item.
+  if cmp.visible() then
+    local entry = cmp.get_selected_entry()
+    if not entry then
+      cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
     end
-  end, {"i","s",}),
-  ['<CR>'] = cmp.mapping(function(fallback)
+    cmp.confirm()
+  else
     fallback()
-  end, {"i","s","c",}),
+  end
+end, { 'i', 's', })
+
+lvim.builtin.cmp.mapping = cmp.mapping.preset.insert({
+  ['<Tab>'] = expandSuggestionMapping,
+  ['<CR>'] = expandSuggestionMapping,
   ['<A-k>'] = cmp.mapping(function(fallback)
     if cmp.visible() then
       cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
     else
       fallback()
     end
-  end, {"i","s","c",}),
+  end, { 'i', 's', 'c', }),
   ['<A-j>'] = cmp.mapping(function(fallback)
     if cmp.visible() then
       cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
     else
       fallback()
     end
-  end, {"i","s","c",}),
+  end, { 'i', 's', 'c', }),
 })
+lvim.builtin.cmp.completion.completeopt = 'menu,menuone,noinsert'
 
 -- Additional plugins.
 lvim.plugins = {
   {
-    "Darazaki/indent-o-matic",
+    'Darazaki/indent-o-matic',
     config = function()
       require('indent-o-matic').setup {
         -- Number of lines without indentation before giving up (use -1 for infinite)
@@ -198,36 +200,36 @@ lvim.plugins = {
       }
     end
   },
-  { "lukas-reineke/indent-blankline.nvim" },
-  { "nvim-treesitter/nvim-treesitter-context" },
-  { "chaoren/vim-wordmotion" },
+  { 'lukas-reineke/indent-blankline.nvim' },
+  { 'nvim-treesitter/nvim-treesitter-context' },
+  { 'chaoren/vim-wordmotion' },
   {
-    "aca/emmet-ls",
+    'aca/emmet-ls',
     config = function()
-      local lspconfig = require("lspconfig")
-      local configs = require("lspconfig/configs")
+      local lspconfig = require('lspconfig')
+      local configs = require('lspconfig/configs')
 
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities.textDocument.completion.completionItem.snippetSupport = true
       capabilities.textDocument.completion.completionItem.resolveSupport = {
         properties = {
-          "documentation",
-          "detail",
-          "additionalTextEdits",
+          'documentation',
+          'detail',
+          'additionalTextEdits',
         },
       }
 
       if not lspconfig.emmet_ls then
         configs.emmet_ls = {
           default_config = {
-            cmd = { "emmet-ls", "--stdio" },
+            cmd = { 'emmet-ls', '--stdio' },
             filetypes = {
-              "html",
-              "css",
-              "javascript",
-              "typescript",
-              "typescriptreact",
-              "javascriptreact",
+              'html',
+              'css',
+              'javascript',
+              'typescript',
+              'typescriptreact',
+              'javascriptreact',
             },
           },
         }
@@ -235,6 +237,7 @@ lvim.plugins = {
       lspconfig.emmet_ls.setup({ capabilities = capabilities })
     end,
   },
+  { 'styled-components/vim-styled-components' }
 }
 
 -- VimScript config for user-defined plugins.
@@ -255,3 +258,4 @@ vim.g.wordmotion_mappings = {
 
 -- Other Vim config.
 vim.opt.colorcolumn = '80'
+vim.opt.clipboard = ''
